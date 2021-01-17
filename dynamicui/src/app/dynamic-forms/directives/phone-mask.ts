a6d9c37eed9ea41176bd295a3e3eec3a389c4946
Chad Michel <cmichel@dontpaniclabs.com>
2021-01-16 21:42:16 -0600

    fix phone directive

--- dynamicui/src/app/dynamic-forms/directives/phone-mask.ts
+++ dynamicui/src/app/dynamic-forms/directives/phone-mask.ts
@@ -26,14 +26,14 @@ export class PhoneMaskDirective {
     if (newVal.length === 0) {
       newVal = '';
     } else if (newVal.length <= 3) {
-      newVal = newVal.replace(/^(\d{0,3})/, '($1)');
+      newVal = newVal.replace(/^(\d{0,3})/, '$1');
     } else if (newVal.length <= 6) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) ($2)');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '$1-$2');
     } else if (newVal.length <= 10) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) ($2)-$3');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '$1-$2-$3');
     } else {
       newVal = newVal.substring(0, 10);
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) ($2)-$3');
+      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '$1-$2-$3');
     }
     this.ngControl.valueAccessor.writeValue(newVal);
   }
